67566a03ad9ecc1f840b8cfcc6212b7628ef5230
Saptarshi Bhattacherya <bhat.bab@gmail.com>
2023-01-21 14:38:21 -0800

    updated models

--- server/models.py
+++ server/models.py
@@ -5,57 +5,26 @@ from sqlalchemy.orm import declarative_base
 Base = declarative_base()
 
 
-class Tester(Base):
-    """A model of a tester."""
-    __tablename__ = 'testers'
-    id = Column(UUID(as_uuid=True), primary_key=True)
-
-    username = Column(String(200))
-    age = Column(SmallInteger())
-    industry = Column(String(200))
-
-    def __repr__(self):
-        return f"Tester<{self.id} | {self.username}>"
-
-
-class Company(Base):
-    """A model of a company."""
-    __tablename__ = 'companies'
+class User(Base):
+    """A model of a user."""
+    __tablename__ = 'users'
     id = Column(UUID(as_uuid=True), primary_key=True)
 
     name = Column(String(200))
     email = Column(String(200))
+    phone = Column(String(12))
+    group_id = Column(UUID(as_uuid=True), primary_key=False)
 
     def __repr__(self):
-        return f"Company<{self.id} | {self.name}>"
+        return f"User<{self.id} | {self.name}>"
 
 
-class Product(Base):
-    """A model of a product."""
-    __tablename__ = 'products'
+class Group(Base):
+    """A model of a group of users."""
+    __tablename__ = 'groups'
     id = Column(UUID(as_uuid=True), primary_key=True)
 
-    company_id = Column(UUID(as_uuid=True), primary_key=False)
     name = Column(String(200))
-    description = Column(String(500))
-    hourly = Column(Numeric(10, 2))
-    target_age = Column(SmallInteger())
-    target_industry = Column(String(200))
-    issues = Column(JSONB)
 
     def __repr__(self):
-        return f"Product<{self.id} | {self.name}>"
-
-
-class Review(Base):
-    """A model of a review."""
-    __tablename__ = 'reviews'
-    id = Column(UUID(as_uuid=True), primary_key=True)
-
-    tester_id = Column(UUID(as_uuid=True), primary_key=False)
-    product_id = Column(UUID(as_uuid=True), primary_key=False)
-    rating = Column(Numeric(10, 2))
-    feedback = Column(String(2000))
-
-    def __repr__(self):
-        return f"Review<{self.id} | {self.rating}>"
+        return f"Group<{self.id} | {self.name}>"
